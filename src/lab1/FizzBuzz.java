ae65513a59f69c30e52369f85e9361160c8aa1cf
zhaniyam <lopolil199@gmail.com>
2026-09-21 10:05:36 -0400

    Replaced for loop with while

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,8 +6,8 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 100; i++) {
+        int i = 1;
+        while (i <= 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +31,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
